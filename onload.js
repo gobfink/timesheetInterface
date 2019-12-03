c6b410c2e749af7fe51778d0687f5a7eae4fe48d
Andy Fortman <gobfink@gmail.com>
2019-12-02 22:23:41 -0500

    basic counter implemented

--- onload.js
+++ onload.js
@@ -1,24 +1,12 @@
-var visit_counter = 0;
-
-console.log("visit_counter", visit_counter)
-
 chrome.storage.local.get("counter", function(visits){
   visit_counter = visits.counter
   console.log("visits: ", visits)
-});
-
-console.log("visit_counter after loaded", visit_counter)
-
-visit_counter++
-console.log("visit_counter after added", visit_counter)
-
-chrome.storage.local.set({"counter": visit_counter }, function () {
+  visit_counter++
+  console.log("visit_counter after added", visit_counter)
+  chrome.storage.local.set({"counter": visit_counter }, function () {
    console.log('Saved visit_counter', visit_counter); 
+   alert('Visit Counter:' + visit_counter)
+  });
 });
 
-chrome.storage.local.get("counter", function(visits){
-  visit_counter = visits.counter
-  console.log("visits2: ", visits)
-});
 
-alert('Visit Counter:' + visit_counter)
